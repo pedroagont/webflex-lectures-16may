4226e9e8cb926e56b3ca74f3d3e6cb6f187a6a45
Pedro A. González <pgonzalez@post.com>
2022-06-14 21:32:27 -0500

    First commit, Networking with TCP and HTTP

---
+++ m02w05/tcp/client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const config = {
+  host: 'localhost',
+  port: 54321
+};
+
+const client = net.createConnection(config);
+
+client.on('data', messageFromServer => {
+  console.log(messageFromServer);
+});
+
+client.setEncoding('utf-8');
+
+// client.write('hey hey hey');
+
+process.stdin.on('data', data => {
+  client.write(data);
+});
